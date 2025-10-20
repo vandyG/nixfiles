c7cdb9692a2151c4f54bb2a0a5da21999e580e9f
vandyG <vandy.goel23@gmail.com>
2025-10-19 23:48:42 -0500

    fix: remove wsl stuff

--- modules/shells.nix
+++ modules/shells.nix
@@ -21,30 +21,9 @@
     '';
   };
 
-  programs.bash = {
-    enable = true;
-    initExtra = ''
-      # If not running interactively, don't do anything
-      if [[ $- != *i* ]]; then
-          return
-      fi
-
-      if [ -e ~/.nix-profile/etc/profile.d/nix.sh ]; then
-        . ~/.nix-profile/etc/profile.d/nix.sh
-      fi
-
-      # If we are not already in fish, start it
-      # The check for FISH_VERSION prevents a loop if fish ever starts a bash shell
-      if command -v fish >/dev/null 2>&1; then
-        exec fish -i
-      fi
-
-    '';
-  };
-
   programs.direnv = {
     enable = true;
-    enableBashIntegration = true; # see note on other shells below
+    enableFishIntegration = true; # see note on other shells below
     nix-direnv.enable = true;
   };
 
