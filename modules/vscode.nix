f99940ae5c6251a02740e9f4a7ae1227406700f2
vandyG <vandy.goel23@gmail.com>
2026-05-11 16:46:44 -0500

    feat: add vscode module

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -1,11 +1,41 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, nix-vscode-extensions, ... }:
+let
+  vscode-marketplace =
+    nix-vscode-extensions.extensions.${pkgs.system}.vscode-marketplace;
+in
 {
     programs.vscode = {
         enable = true;
         profiles = {
-            "personal" = {
+            "default" = {
                 enableMcpIntegration = true;
-            }
+                enableUpdateCheck = true;
+                extensions = with pkgs.vscode-extensions; [
+                    eamodio.gitlens
+                    ms-python.debugpy
+                    ms-python.python
+                    ms-python.vscode-pylance
+                    ms-toolsai.jupyter
+                    ms-toolsai.jupyter-keymap
+                    ms-toolsai.jupyter-renderers
+                    ms-toolsai.vscode-jupyter-slideshow
+                    ms-toolsai.vscode-jupyter-cell-tags
+                    github.copilot-chat
+                    jnoortheen.nix-ide
+                    mkhl.direnv
+                ]
+                ++
+                (
+                    with vscode-marketplace; [
+                        # Marketplace-only extensions
+                        ms-python.vscode-python-envs
+                        ms-toolsai.vscode-jupyter-powertoys
+                        ms-vscode.vscode-websearchforcopilot
+                        ms-vscode.vscode-copilot-vision
+                    ]
+                )
+                ;
+            };
         };
     };
 }
