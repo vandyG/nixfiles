1ffbffd95d7cab63631212a78ccb0329b235eece
vandyG <vandy.goel23@gmail.com>
2026-05-12 03:08:23 -0500

    refactor: VSCode extensions and user settings for improved readability

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -13,33 +13,31 @@ in
       "default" = {
         enableMcpIntegration = true;
         enableUpdateCheck = true;
-        extensions = with pkgs.vscode-extensions; [
-          eamodio.gitlens
-          ms-python.debugpy
-          ms-python.python
-          ms-python.vscode-pylance
-          ms-toolsai.jupyter
-          ms-toolsai.jupyter-keymap
-          ms-toolsai.jupyter-renderers
-          ms-toolsai.vscode-jupyter-slideshow
-          ms-toolsai.vscode-jupyter-cell-tags
-          github.copilot-chat
-          jnoortheen.nix-ide
-          charliermarsh.ruff
-          catppuccin.catppuccin-vsc
-          catppuccin.catppuccin-vsc-icons
-        ]
-        ++
-        (
-          with vscode-marketplace; [
+        extensions =
+          with pkgs.vscode-extensions;
+          [
+            eamodio.gitlens
+            ms-python.debugpy
+            ms-python.python
+            ms-python.vscode-pylance
+            ms-toolsai.jupyter
+            ms-toolsai.jupyter-keymap
+            ms-toolsai.jupyter-renderers
+            ms-toolsai.vscode-jupyter-slideshow
+            ms-toolsai.vscode-jupyter-cell-tags
+            github.copilot-chat
+            jnoortheen.nix-ide
+            charliermarsh.ruff
+            catppuccin.catppuccin-vsc
+            catppuccin.catppuccin-vsc-icons
+          ]
+          ++ (with vscode-marketplace; [
             # Marketplace-only extensions
             ms-python.vscode-python-envs
             ms-toolsai.vscode-jupyter-powertoys
             ms-vscode.vscode-websearchforcopilot
             ms-vscode.vscode-copilot-vision
-          ]
-        )
-        ;
+          ]);
         userMcp = {
           servers = {
             "nixos" = {
@@ -53,49 +51,49 @@ in
           };
         };
         userSettings = {
-          "workbench.iconTheme"= "catppuccin-mocha";
-          "workbench.colorTheme"= "Catppuccin Mocha";
-          "terminal.integrated.fontFamily"= "JetBrainsMono NF";
-          "ruff.enable"= true;
-          "ruff.configuration"= "/home/vandy/work/.vscode/ruff.toml";
-          "notebook.editorOptionsCustomizations"= {
-              "editor.tabSize"= 4;
-              "editor.indentSize"= 4;
-              "editor.insertSpaces"= true;
+          "workbench.iconTheme" = "catppuccin-mocha";
+          "workbench.colorTheme" = "Catppuccin Mocha";
+          "terminal.integrated.fontFamily" = "JetBrainsMono NF";
+          "ruff.enable" = true;
+          "ruff.configuration" = "/home/vandy/work/.vscode/ruff.toml";
+          "notebook.editorOptionsCustomizations" = {
+            "editor.tabSize" = 4;
+            "editor.indentSize" = 4;
+            "editor.insertSpaces" = true;
           };
-          "diffEditor.codeLens"= true;
-          "[python]"= {
-            "editor.defaultFormatter"= "charliermarsh.ruff";
-            "editor.formatOnSave"= true;
+          "diffEditor.codeLens" = true;
+          "[python]" = {
+            "editor.defaultFormatter" = "charliermarsh.ruff";
+            "editor.formatOnSave" = true;
           };
-          "remote.autoForwardPortsSource"= "hybrid";
-          "workbench.editorAssociations"= {
-            "*.mp3"= "vscode.audioPreview";
+          "remote.autoForwardPortsSource" = "hybrid";
+          "workbench.editorAssociations" = {
+            "*.mp3" = "vscode.audioPreview";
           };
-          "jupyter.askForKernelRestart"= false;
-          "[nix]"= {
-            "editor.insertSpaces"= true;
-            "editor.tabSize"= 2;
+          "jupyter.askForKernelRestart" = false;
+          "[nix]" = {
+            "editor.insertSpaces" = true;
+            "editor.tabSize" = 2;
           };
-          "files.autoSave" = "off"; 
-          "nix.enableLanguageServer"= true;
-          "nix.serverPath"= "nixd";
-          "nix.serverSettings"= {
-            "nixd"= {
-              "formatting"= {
-                "command"= [
+          "files.autoSave" = "off";
+          "nix.enableLanguageServer" = true;
+          "nix.serverPath" = "nixd";
+          "nix.serverSettings" = {
+            "nixd" = {
+              "formatting" = {
+                "command" = [
                   "nixfmt"
                 ];
               };
-              "nixpkgs"= {
-                "expr"= "import <nixpkgs> { }";
+              "nixpkgs" = {
+                "expr" = "import <nixpkgs> { }";
               };
-              "options"= {
-                "nixos"= {
-                  "expr"= "(builtins.getFlake (builtins.toString ./.)).nixosConfigurations.vandy.options";
+              "options" = {
+                "nixos" = {
+                  "expr" = "(builtins.getFlake (builtins.toString ./.)).nixosConfigurations.vandy.options";
                 };
-                "home-manager"= {
-                  "expr"= "(builtins.getFlake (builtins.toString ./.)).homeConfigurations.vandy-nixos.options";
+                "home-manager" = {
+                  "expr" = "(builtins.getFlake (builtins.toString ./.)).homeConfigurations.vandy-nixos.options";
                 };
               };
             };
