7162217042a29e940bdc0cadbd98d62955d2b4cd
vandyG <vandy.goel23@gmail.com>
2026-06-16 15:34:12 -0500

    refactor: update firefox configuration to include profile settings and path

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -1,17 +1,24 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}:
+{ config, ... }:
 {
   programs.firefox = {
     enable = true;
+    configPath = "${config.xdg.configHome}/mozilla/firefox";
     profiles = {
       default = {
         id = 0;
         name = "default";
         isDefault = true;
+        settings = {
+          "sidebar.verticalTabs" = true;
+          "SanitizeOnShutdown" = {
+            "Cache" = true;
+            "Cookies" = true;
+            "FormData" = true;
+            "Sessions" = true;
+            "SiteSettings" = true;
+            "Locked" = true;
+          };
+        };
       };
     };
   };
