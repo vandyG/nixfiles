d80765d43c0fecda5a0edea60bd484b94303dedf
vandyG <vandy.goel23@gmail.com>
2026-06-16 16:16:30 -0500

    feat: change bootloader to limine

--- system/vandy/configuration.nix
+++ system/vandy/configuration.nix
@@ -2,18 +2,24 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, self, ... }:
+{
+  config,
+  pkgs,
+  self,
+  ...
+}:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ./hardware/asus.nix
-      ./hardware/nvidia.nix
-    ];
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ./hardware/asus.nix
+    ./hardware/nvidia.nix
+  ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.limine.enable = true;
+  # boot.loader.systemd-boot.enable = true;
   # Shared ESP is very small on this triple-boot machine.
   boot.loader.systemd-boot.configurationLimit = 1;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -87,9 +93,12 @@
   users.users.vandy = {
     isNormalUser = true;
     description = "Vandit Goel";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
     packages = with pkgs; [
-    #  thunderbird
+      #  thunderbird
     ];
   };
 
@@ -105,8 +114,9 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    #  wget
+    sbctl
   ];
 
   programs.steam.enable = true;
@@ -142,5 +152,5 @@
   system.configurationRevision = self.rev or self.dirtyShortRev or "dirty";
 
   system.stateVersion = "25.11"; # Did you read the comment?
-  
+
 }
