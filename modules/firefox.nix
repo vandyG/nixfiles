ebe0eab3e134dd29304a3fecc36f4db0575c4bc5
vandyG <vandy.goel23@gmail.com>
2026-06-20 01:14:35 -0500

    feat: add search engine policy for Nix Packages in Firefox configuration

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -4,6 +4,19 @@
     enable = true;
     configPath = ".mozilla/firefox";
     # configPath = "${config.xdg.configHome}/mozilla/firefox";
+    policies = {
+      "SearchEngines" = {
+        "Add" = [
+          {
+            "Name" = "Nix Packages";
+            "URLTemplate" = "https://search.nixos.org/packages?channel=unstable&q={searchTerms}";
+            "Method" = "GET";
+            "Alias" = "@nix";
+            "Description" = "Search Nix Packages on the unstable channel";
+          }
+        ];
+      };
+    };
     profiles = {
       default = {
         id = 0;
